411c04f4e89a3e680bec6f6c5c943d2a339ce9e1
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2022-12-28 00:47:50 -0300

    Add tsconfig paths
    - Add paths @entities, @database, @repositories and @useCases
    - Configure tsconfig paths in jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
 export default {
   clearMocks: true,
   coverageProvider: 'v8',
   preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>'
+  }),
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/']
 }
