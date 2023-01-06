2c116558d0baef9e1f6b13c8a5e4abfd1b647def
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2023-01-06 13:52:25 -0300

    add in memory users repository

---
+++ src/repositories/inMemory/inMemoryUsersRepository.spec.ts
@@ -0,0 +1,170 @@
+import { InMemoryUsersRepository } from './inMemoryUsersRepository'
+import { User } from '@entities/user'
+
+describe('In memory users repository tests', () => {
+  it('should be able to create an user', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    const createdUser = await usersRepository.getUserById(user.id)
+
+    expect(createdUser).toBeInstanceOf(User)
+    expect(createdUser).toEqual(user)
+  })
+
+  it('should be able to get a password with a valid user email', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    const password = await usersRepository.getUserPassword(user.email)
+
+    expect(password).toBeTruthy()
+    expect(password).toEqual(user.password)
+  })
+
+  it('should not be able to get a password with an invalid user email', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const password = await usersRepository.getUserPassword(
+      'invalidEmail@invalid.com'
+    )
+
+    expect(password).toBeNull()
+  })
+
+  it('should be able to get an user by id', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    const gettedUser = await usersRepository.getUserById(user.id)
+
+    expect(gettedUser).toBeTruthy()
+  })
+
+  it('should not be able to get an user with an invalid id', async () => {
+    const usersRepository = InMemoryUsersRepository()
+
+    const gettedUser = await usersRepository.getUserById('invalid id')
+
+    expect(gettedUser).toBeNull()
+  })
+
+  it('should be able to get an user by email', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    const gettedUser = await usersRepository.getUserByEmail(user.email)
+
+    expect(gettedUser).toBeTruthy()
+  })
+
+  it('should not be able to get an user with an invalid email', async () => {
+    const usersRepository = InMemoryUsersRepository()
+
+    const gettedUser = await usersRepository.getUserByEmail(
+      'invalid@invalid.com'
+    )
+
+    expect(gettedUser).toBeNull()
+  })
+
+  it('should be able to edit an user', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    const editUserData = {
+      id: user.id,
+      name: 'Mark',
+      lastName: 'Zuckerberg',
+      password: 'aliens1234'
+    }
+
+    const editedUser = await usersRepository.editUser(editUserData)
+    const editedUserPassword = await usersRepository.getUserPassword(
+      editedUser.email
+    )
+
+    expect(editedUser).toBeTruthy()
+    expect(editedUser.name).toEqual(editUserData.name)
+    expect(editedUser.lastName).toEqual(editUserData.lastName)
+    expect(editedUser.email).toEqual(user.email)
+    expect(editedUserPassword).toEqual(editUserData.password)
+  })
+
+  it('should not be able to edit an user with invalid id given', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const editedUser = await usersRepository.editUser({ id: 'invalid id' })
+
+    expect(editedUser).toBeNull()
+  })
+
+  it('should be able to delete an user', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    await usersRepository.deleteUser(user.id)
+
+    const gettedUser = await usersRepository.getUserById(user.id)
+
+    expect(gettedUser).toBeNull()
+  })
+
+  it('should not be able to delete an user with an invalid id given', async () => {
+    const usersRepository = InMemoryUsersRepository()
+    const user = new User({
+      name: 'Joe',
+      lastName: 'Doe',
+      email: 'joe.doe@exemple.com',
+      password: '123456'
+    })
+
+    await usersRepository.createUser(user)
+
+    await usersRepository.deleteUser('invalid id')
+
+    const gettedUser = await usersRepository.getUserById(user.id)
+
+    expect(gettedUser).toBeTruthy()
+  })
+})
---
+++ src/repositories/inMemory/inMemoryUsersRepository.ts
@@ -0,0 +1,49 @@
+import { User } from '@entities/user'
+import { IUsersRepository, editUserDTO } from '@repositories/IUsersRepository'
+
+export const InMemoryUsersRepository = (): IUsersRepository => {
+  let users: User[] = []
+
+  const getUserPassword = async (email: string): Promise<string | null> => {
+    const user = users.find(user => user.email === email)
+
+    return user ? user.password : null
+  }
+  const createUser = async (user: User): Promise<void> => {
+    users.push(user)
+  }
+  const getUserById = async (userId: string): Promise<User | null> => {
+    return users.find(user => user.id === userId) || null
+  }
+  const getUserByEmail = async (userEmail: string): Promise<User | null> => {
+    return users.find(user => user.email === userEmail) || null
+  }
+  const editUser = async ({
+    id,
+    name,
+    lastName,
+    password
+  }: editUserDTO): Promise<User | null> => {
+    const user = await getUserById(id)
+
+    if (!user) return null
+
+    user.name = name ?? user.name
+    user.lastName = lastName ?? user.lastName
+    user.password = password ?? user.password
+
+    return user
+  }
+  const deleteUser = async (userId: string): Promise<void> => {
+    users = users.filter(user => user.id !== userId)
+  }
+
+  return Object.freeze({
+    getUserPassword,
+    createUser,
+    getUserById,
+    getUserByEmail,
+    editUser,
+    deleteUser
+  })
+}
